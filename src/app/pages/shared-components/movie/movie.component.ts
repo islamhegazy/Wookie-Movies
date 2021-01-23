665b95e7d86c204ec5c9eb7279fae661d1f6c336
islam hegazy <islam.ab.hegazy@gmail.com>
2021-01-23 13:13:35 +0200

    1/23/2021

--- src/app/pages/shared-components/movie/movie.component.ts
+++ src/app/pages/shared-components/movie/movie.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { WookieMovie } from '../../../logic/models/wookie-movie.model';
-import { WookieMoviesService } from '../../../logic/services/wookie-movies.service';
 
 @Component({
   selector: 'app-movie',
